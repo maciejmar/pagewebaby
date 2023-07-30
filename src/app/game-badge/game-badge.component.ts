c7a28e311d63c99879e98f46dcbb599349bae15d
Maciej Marosz <48849533+maciejmar@users.noreply.github.com>
2023-07-30 22:12:46 +0200

    changes in badges,cards and gamejson added

--- src/app/game-badge/game-badge.component.ts
+++ src/app/game-badge/game-badge.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import {Game} from '../game'
+
 
 @Component({
   selector: 'app-game-badge',
   templateUrl: './game-badge.component.html',
-  styleUrls: ['./game-badge.component.css']
+  styleUrls: ['./game-badge.component.scss']
 })
 export class GameBadgeComponent implements OnInit {
 
-  constructor() { }
+  gamesjson:Game[]=[]
+  constructor() {}
+
 
   ngOnInit(): void {
+
+      this.gamesjson =[ {
+      name: "Nicpoń.Zaginione literki",
+      category: [
+        "Przygodowa",
+        "Edukacyjna"
+      ],
+      age:"7-100",
+      image: "",
+      description:"Przygody chłopca, który ratuje swoją krainę przed zagładą. Podczas brawurowej wyprawy po zaginione litery sam doznaje przemiany w dobrego, odważnego młodzieńca.",
+      price:12,
+      createdAt: Date.now()
+      },
+      {
+       name: "Kto złapie miśka Lutka",
+        category: [
+          "Arcade",
+          "Zręcznościowa"
+        ],
+        age:"4",
+        image:"",
+        description:"Niedźwiedź Lutek wybiera się w niebezpieczną podróż by zdobyć pożywienie dla swej rodziny. Po drodze spotykają go różne przygody, w których musi wykazać się zręcznością i pomysłowością.",
+        price:12,
+        createdAt: Date.now()
+      },
+      {
+        name: "Rybki uciekają",
+        category: [
+          "Sensoryczna",
+          "Zręcznościowa"
+        ],
+        age:"3",
+        image:"",
+        description:"Polem gry jest akwarium podzielone na kwadraty. W kolejnych kwadratach pojawiają się rybki, którymi trzeba kierować, żeby trafiły tam gdzie chcemy. Gracz wygrywa, jeśli uda mu się zagonić wszystkie rybki do domku-muszli.",
+        price:12,
+        createdAt: Date.now()
+      }
+
+
+    ]
   }
 
 }
---
+++ src/app/game.ts
@@ -0,0 +1,13 @@
+
+export interface Game {
+  name:string;
+  category:string[];
+  age:string;
+  image: string;
+  description:string;
+  price:number;
+  createdAt: number;
+
+
+}
+
