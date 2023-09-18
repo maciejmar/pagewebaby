50c559c1f9e6d8abece839c8f33cf124b267a361
maciejmar <memgos@gmail.com>
2023-09-18 11:35:14 +0200

    change background of game on black

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import AOS from 'aos';
 
 @Component({
@@ -8,6 +8,7 @@ import AOS from 'aos';
 })
 export class FooterComponent implements OnInit {
   visible = false;
+  @Input () bgClass: string = 'default-bg';
   constructor() { }
 
   ngOnInit(): void {
