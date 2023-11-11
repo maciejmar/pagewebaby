58ce3668b146c25cbd230fd7c9e3773e966d8672
maciejmar <memgos@gmail.com>
2023-11-12 00:47:15 +0100

    changing abcland screen and font on Oswald

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -26,8 +26,8 @@ export class GameComponent implements AfterViewInit {
   @ViewChild('target') target!: ElementRef;
   show: boolean = false;
   buttonsState: 'hidden' | 'visible' = 'hidden';
-  grayImage = './../../assets/nicpon-11-3.png';
-  colorImage = './../../assets/nicpon-11black.png';
+  grayImage = './../../assets/runningNicponColor.png';
+  colorImage = './../../assets/runningNicponbw.png';
   maskHeight = 0;
   imageWidth = 100;
   imageHeight: number; // This depends on the aspect ratio
