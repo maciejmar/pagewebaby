4a4dc9d0cb9ffc3c7e4aab648a952f35348fe791
maciejmar <memgos@gmail.com>
2025-08-06 16:17:35 +0200

    removing path in routing for pwa to game

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,7 +22,6 @@ const routes: Routes = [
     { path: 'basketball-shots', component: BasketballShotsComponent },
     { path: 'logo', component: IntroComponent },
     { path: 'game', component: GameComponent },
-    { path: 'basketball-pwa', component: GameComponent},
     { path: 'abecadlowo', component: AppComponent },
     //{ path: 'app-ads.txt', component:AppComponent},
     { path: '**', component: PagenotfoundComponent },
