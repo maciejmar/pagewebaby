de30ddc2863b6c8ab9e88f13ac6bebc9db5dd900
maciejmar <memgos@gmail.com>
2025-08-07 09:21:41 +0200

    scss instead css in path of basketball-pwa component corrected

--- src/app/basketball-pwa/basketball-pwa.component.ts
+++ src/app/basketball-pwa/basketball-pwa.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-basketball-pwa',
   templateUrl: './basketball-pwa.component.html',
-  styleUrls: ['./basketball-pwa.component.css']
+  styleUrls: ['./basketball-pwa.component.scss']
 })
 export class BasketballPwaComponent implements OnInit {
 
